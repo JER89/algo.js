831afb0c8bf7c736fc886775731a05a2f1dd5169
Jeremy Mertens <mertensjeremy89@gmail.com>
2021-01-18 16:37:22 +0100

    7

---
+++ ex1_6.js
@@ -0,0 +1,7 @@
+
+const readlineSync = require("readline-sync");
+
+let Num1 = new number (readlineSync.question("Number is big"));
+let Num2 = new number (readlineSync.qestion ("Number is small"));
+
+console.log(Num1 / Num2 - Math.floor(Num1/Num2));
