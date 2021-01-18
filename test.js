99de8d2f0d93c69f4caa3b4607aad3f87bc8e63f
Jeremy Mertens <mertensjeremy89@gmail.com>
2021-01-18 14:26:36 +0100

    ex1.0

---
+++ test.js
@@ -0,0 +1,12 @@
+
+console . log ( "Ça marche !!!" ) ;
+
+const readlineSync = require("readline-sync");
+
+let userName = readlineSync.question('Can you give me your name please?');
+console.log("Hello " + userName);
+
+let age = 25; //I'm 25
+age =36; // I get older
+age = 18 // Miraculously I'm getting younger
+
