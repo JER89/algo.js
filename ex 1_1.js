797a9d0735a18ea0e7bfa0d27ef3b970c167a377
Jeremy Mertens <mertensjeremy89@gmail.com>
2021-01-18 15:49:14 +0100

    1_1

---
+++ ex 1_1.js
@@ -0,0 +1,25 @@
+
+
+const readlineSync = require("readline-sync");
+
+let userName = readlineSync.question('Can you give me your name please?');
+console.log("Hello " + userName);
+
+let age = 25; //I'm 25
+age =36; // I get older
+age = 18 // Miraculously I'm getting younger
+
+console.log(`age${age}`);
+
+let  htmlBadges  =  new  Number ( readlineSync . question ( 'Combien de badges HTML avez-vous?' ) ) ; 
+
+let  cssBadges  =  new  Number ( readlineSync . question ( 'Combien de badges CSS avez-vous?' ) ) ; 
+
+laissez  totalBadges  =  htmlBadges  +  cssBadges ; 
+
+console . log ( 'Woaw, vous avez'  +  totalBadges  +  "!" ) ;
+
+
+let name = "Jérémy";
+let age = 32;
+console.log( "name" + name + "age" + age);
--- test.js
+++ test.js
@@ -1,12 +1,36 @@
-
-console . log ( "Ça marche !!!" ) ;
-
 const readlineSync = require("readline-sync");
+
+let phrase ="Coding is fun !";
+
+let isItTrue = true;
+let isItFalse = false;
+
+let age = 23 ;
+
+let sisterAge = age + 11;
+
+age += 12; // age= age + 12;
+
+console.log(age);
+
+let name = "Jean";
+let nameSister = "Emma";
+
+console.log("My name is" + name);
 
 let userName = readlineSync.question('Can you give me your name please?');
 console.log("Hello " + userName);
 
-let age = 25; //I'm 25
-age =36; // I get older
-age = 18 // Miraculously I'm getting younger
+let htmlBadges = new Number(readlineSync.question('How many HTML badges do you have?'));
+
+let cssBadges = new Number(readlineSync.question('How many CSS badges do you have?'));
+
+let totalBadges = htmlBadges + cssBadges;
+
+console.log('Woaw, you have ' + totalBadges + "!");
+
+
+
+
+
 
