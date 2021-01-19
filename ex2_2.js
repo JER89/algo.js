32a98256fa2fc05d6ab755d7e33af65779a1af98
Jeremy Mertens <mertensjeremy89@gmail.com>
2021-01-19 10:37:02 +0100

    22

---
+++ ex2_2.js
@@ -0,0 +1,16 @@
+
+const readlineSync = require("readline-sync");
+
+let min = readlineSync.question("What's your minimum number ? ");
+let max = readlineSync.question("What's your maximum number ?");
+let current = readlineSync.question("What's your current number ?");
+
+if (((Number(current)) >= (Number(min))) && ((Number(current)) <= (Number(max)))) {
+    console.log(current + "is between " + min + "and " + max)
+} else {
+    console.log(current + "is not between " + min + "and "+ max)
+}
+
+if ((Number(min)) > (Number(max))) {
+    console.log ("Sorry! Your request is not valid...")
+}
