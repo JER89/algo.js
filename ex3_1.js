36f727ce25565c119a1d34a136a6504fbbb840e2
Jeremy Mertens <mertensjeremy89@gmail.com>
2021-01-20 10:14:53 +0100

    32

--- ex3_1.js
+++ ex3_1.js
@@ -1,17 +1,12 @@
 
 const readlineSync = require("readline-sync");
 
-/* let a = [1,2,3,4,5];
-let sum = a.reduce(function(a , b) {
-    return a + b ; }, 0);
-
-    console.log(sum); */
-
 
 let array = [1,2,3,4,5];
 let reducer = ( accumulator, currentValue) => accumulator + currentValue;
 
 console.log(array.reduce(reducer));
+
 
 
 
---
+++ ex3_2.js
@@ -0,0 +1,5 @@
+
+
+
+let average = (array) => array.reduce((a, b) => a + b) / array.length;
+console.log(average([1,2,3,4,5]));
