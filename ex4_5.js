d65bba49713a1174911873a4546453100ddb677b
Jeremy Mertens <mertensjeremy89@gmail.com>
2021-01-20 15:04:48 +0100

    451

--- ex4_5.js
+++ ex4_5.js
@@ -5,16 +5,6 @@ let xB = 3;
 let yA = 1;
 let yB = 2;
 
-/* function calcDistance(xA, yA, xB, yB) {
-
-    let dX = xA - xB;
-    let dY = yA - yB;
-
-    let distance = Math.sqrt((Math.pow(dX,2))+(Math.pow(dY,2)));
-
-    return distance;
-} */
-
 function calcDistance(xA, yA, xB, yB) {
 
 return Math.sqrt(((yB - yA) * (yB - yA)) + ((xA - xB) * (xA-xB)));
@@ -22,3 +12,4 @@ return Math.sqrt(((yB - yA) * (yB - yA)) + ((xA - xB) * (xA-xB)));
 
 
 console.log("distance = " + calcDistance(xA, yA, xB, yB))
+
