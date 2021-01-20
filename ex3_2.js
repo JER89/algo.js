dae3f82bbf595a4e93648eef9fd2536fc6bfa4f1
Jeremy Mertens <mertensjeremy89@gmail.com>
2021-01-20 10:22:04 +0100

    322

--- ex3_2.js
+++ ex3_2.js
@@ -1,7 +1,11 @@
 
 
 
+/* let average = (array) => array.reduce((a, b) => a + b) / array.length;
+console.log(average([1,2,3,4,5])); */
+
 let average = (array) => array.reduce((a, b) => a + b) / array.length;
-console.log(average([1,2,3,4,5]));
+console.log(average([100,101,102]));
+
 
 
