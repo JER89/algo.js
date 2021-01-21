80c54c9af28382989febf6886560c60a48fb0577
Jeremy Mertens <mertensjeremy89@gmail.com>
2021-01-21 14:48:44 +0100

    621

--- ex6_2.js
+++ ex6_2.js
@@ -10,27 +10,19 @@ class Rectangle {
 
     collides(otherRectangle) {
        
-        let collideRightLeft = 
-        this.topLeftXPos + this.length >= otherRectangle.topLeftXPos &&
-        otherRectangle.topLeftXPos + otherRectangle.length >= this.topLeftXPos;
-
-        /*let collideTopBottom =
-        this.topLeftXPos - this.width >= otherRectangle.topLeftXPos &&
-        otherRectangle.topLeftXPos - otherRectangle.width >= this.topLeftXPos;
-
-        return collisionRightLeft && collisionTopBottom*/
-
-        let collideTopBottom = 
-        this.topLeftYPos >= otherRectangle.topLeftYPos - otherRectangle.width &&
-        otherRectangle.topLeftYPos >= this.topLeftYPos - this.width;
-
-        return collideRightLeft && collideTopBottom
+        if(((this.topLeftXPos === otherRectangle.topLeftXPos) || 
+           (this.topLeftYPos === otherRectangle.topLeftYPos)) || 
+           ((this.width === otherRectangle.width) || 
+           (this.length === otherRectangle.length))) {
+               return true;
+           }
+               return false;
+           
     
     }
 }
 
 let Rectangle1 = new Rectangle(2,6,2,6);
-let Rectangle2 = new Rectangle (3,5,3,5);
-let Both = Rectangle1.collides(Rectangle2);
+let Rectangle2 = new Rectangle(3,5,3,5);
 
-console.log(Both);
+console.log(Rectangle1.collides(Rectangle2));
